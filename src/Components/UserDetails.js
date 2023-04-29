c2bc9cad6a1be90ac3a8a09e6eccb73487cdc32f
Tejas204 <tejasdhopavkar2000@gmail.com>
2023-04-29 09:46:13 +0530

    Used the useParam() hook to get the params of the URL

--- src/Components/UserDetails.js
+++ src/Components/UserDetails.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const UserDetails = () => {
+
+  // React router dom provides us with a hook called useParams which can be used to make API calls
+  // In this scenario, it will be used to get the URL.
+  // This hook returns a key value pair
+  const params = useParams();
+
+  // The params.userId is the userId param that we passed when creating the route
+  const userId = params.userId;  
+
   return (
-    <div>USer Details</div>
+    <div>USer Details: {userId}</div>
   )
 }
 
