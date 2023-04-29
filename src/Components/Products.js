6c53c54b1cf26111638b1e47ecf0df33bac0c824
Tejas204 <tejasdhopavkar2000@gmail.com>
2023-04-29 10:12:11 +0530

    Added explanation of related links

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -10,6 +10,10 @@ const Products = () => {
 
         <nav className='secondary-nav'>
             {/* Note: we dont use / for nested routes as of now */}
+            {/* This is because they are related links. They inherit the path of the closest route in which they are rendered. */}
+            {/* As featured and new are nested in products, their paths are products/featured and products/new respectively. */}
+            {/* However, if we user /new and /featrued; they will be viewed as children of the root instead of products. This will
+                trigger the no match route */}
 
             {/* Even though we created nested routes in App.js; React does not know where to render the components.
                 For this, we have a component called as 'Outlet'; added just below the Link component*/}
